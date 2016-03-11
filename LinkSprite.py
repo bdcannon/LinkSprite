af3009bfc64857b6f9df69a4e0ac371305b4ab10
Ben Cannon <cannon.benjamind@gmail.com>
2016-03-10 16:46:24 -0800

    End of day. Most Tests passing

--- LinkSprite.py
+++ LinkSprite.py
@@ -1,7 +1,98 @@
 """
   This class
 """
+import time
+
 class LinkSprite :
+    rst_cmd = b'\x56\x00\x26\x00'
+    rst_resp = b'v\x00&\x00\x00VC0703 1.00\r\nCtrl infr exist\r\nUser-defined sensor\r\n625\r\nInit end\r\n'
+
+    pic_cmd = b'\x56\x00\x36\x01\x00'
+    pic_resp = b'\x76\x00\x36\x00\x00'
+
+    pic_size_cmd = b'\x56\x00\x34\x01\x00'
+    pic_size_resp = b'\x76\x00\x34\x00\x04\x00\x00\xFF\xFF'
+
+    # Command for compression ratio
+    comp_cmd = b'\x56\x00\x31\x05\x01\x01\x12\x04'
+    comp_resp = b'\x76\x00\x31\x00\x00'
+
+
     def __init__(self, serial):
-        
-        self.defBaud = 38400
+        self.serial = serial
+        self.clearPort()
+
+    def waitForInit(self) :
+        self.serial.write(self.pic_cmd)
+        resp = self.serial.read(67)
+
+        while not resp.strip().endswith(b'Init end') :
+            time.sleep(3)
+            self.serial.read(self.serial.in_waiting)
+            self.serial.write(self.pic_cmd)
+            resp = self.serial.read(67)
+
+    def reset(self) :
+        """
+        Tries to reset the camera
+        """
+        self.serial.write(self.rst_cmd)
+        resp_data = self.serial.read(len(self.rst_resp))
+
+        if resp_data == self.rst_resp :
+            print('Camera Reset')
+
+        return (len(resp_data), resp_data)
+
+    def setCompression(self, compression) :
+        """
+        Sets the compression ration for the jpg image. Expecting values of
+        0 - 255
+        """
+        if compression > 255 :
+            compression = 255
+
+        comp_bytes = compression.to_bytes(1, byteorder='big')
+        print(list(self.comp_cmd + comp_bytes))
+        self.serial.write(self.comp_cmd + comp_bytes)
+
+        resp = self.serial.read(len(self.comp_resp) + 2)
+        print('Compression resp')
+        print(list(resp))
+        resp_comp = int.from_bytes(resp[-1:], byteorder='big')
+        print(resp_comp)
+
+        return(resp_comp, resp)
+
+
+    # Try to clear anything thats in the serialPort buffer
+    def clearPort(self):
+        """
+        Tries to empty the buffer for the camera. Just in case there's any garbage
+        """
+        while self.serial.in_waiting :
+            self.serial.read(self.serial.in_waiting)
+        print('Bytes in waiting %d' % self.serial.in_waiting)
+
+    def getWaiting(self):
+        """
+        Returns how many bytes are waiting to be read in the serial buffer.
+        """
+        return self.serial.in_waiting
+
+    def getPicSize(self) :
+        """
+        Return the size of the picture that is sitting in the buffer of the
+        camera. Read this many bytes later
+        """
+        self.serial.write(self.pic_size_cmd)
+        resp = self.serial.read(len(self.pic_size_resp))
+        size = int.from_bytes(resp[-2:], byteorder='big')
+        return (size, resp)
+
+    def takePic(self):
+        self.serial.write(self.pic_cmd)
+        resp = self.serial.read(len(self.pic_resp))
+        if resp == self.pic_resp :   # Successful
+            print('Snap!')
+        return(len(resp), resp)
--- testLinksprite.py
+++ testLinksprite.py
@@ -1,16 +1,19 @@
 import unittest
 import serial
 from LinkSprite import LinkSprite
+import time
+
+serial = serial.Serial('/dev/ttyUSB0', 38400, timeout=1)
 
 class TestLinkSprite(unittest.TestCase):
     def setUp(self):
-        self.ls = LinkSprite()
+        self.ls = LinkSprite(serial)
+        self.ls.clearPort()
+        self.ls.reset()
+        time.sleep(1)
 
     def testClassInitiation(self):
-        self.ls = LinkSprite()
         self.assertFalse(self.ls is None)
-
-      # Need to check fields
 
     def testReset(self) :
         # Returns
@@ -19,8 +22,14 @@ class TestLinkSprite(unittest.TestCase):
         resp = self.ls.reset()
         self.assertTrue(resp[0] > 0) # Response length is greater than 0 bytes
         self.assertEqual(resp[1], rst_resp)
-        self.assertEqual(ls.getWaiting(), 0)   # Check how many bytes are available
+        self.assertEqual(self.ls.getWaiting(), 0)   # Check how many bytes are available
 
+    def testGetPicSize(self) :
+        self.ls.takePic()
+        resp =  self.ls.getPicSize()
+
+        self.assertIsInstance(resp[0], int)
+        self.assertTrue(resp[0] > 0 and resp[0] <= 65535)
 
     def testGetImageSize(self):
         sizeBase = b'\x56\x00\x31\x05\x04\x01\x00'
@@ -36,16 +45,21 @@ class TestLinkSprite(unittest.TestCase):
         resp = self.ls.takePic()
 
         self.assertTrue(resp[0] > 0) # Make sure we got some more bytes
-        self.asserIsInstance(resp[1], bytes)
+        self.assertIsInstance(resp[1], bytes)
         self.assertEqual(resp[0], len(resp[1]))
 
     def testCompressionRatio(self):
-        comp_resp = b'\x56\x00\x31\x05\x01\x01\x12\x04\x36'
-
         resp = self.ls.setCompression(54)
 
+        print(resp[0])
+        print(resp[1])
+
         self.assertTrue(resp[0] > 0)
-        self.assertEqual(resp[1], comp_resp)
+        self.assertTrue(resp[0] == 54)
+
+        resp = self.ls.setCompression(255)
+        self.assertTrue(resp[0] > 0)
+        self.assertTrue(resp[0] == 255)
 
 if __name__ == '__main__':
   unittest.main()
