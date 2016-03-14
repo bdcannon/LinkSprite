5891dd2db72f5bc401ae1e8fc2f1bb9de715ec5c
Ben Cannon <cannon.benjamind@gmail.com>
2016-03-14 10:36:48 -0700

    Seems to be working pretty well

--- LinkSprite.py
+++ LinkSprite.py
@@ -17,10 +17,44 @@ class LinkSprite :
     comp_cmd = b'\x56\x00\x31\x05\x01\x01\x12\x04'
     comp_resp = b'\x76\x00\x31\x00\x00'
 
+    pic_dim_cmd = b'\x56\x00\x31\x05\x04\x01\x00\x19' # still needs additional byte
+    pic_dim_resp = b'\x76\x00\x31\x00\x00'
+
+    # Read the buffer content
+    # read_cont + address(H L) + read_cont_sep + read_size + read_cont_delay
+    read_cont = b'\x56\x00\x32\x0C\x00\x0A\x00\x00'
+    read_cont_sep = b'\x00\x00'
+    read_size = b'\x0C\x80'
+    read_size_inc = int.from_bytes(read_size, byteorder='big')
+    read_cont_delay = b'\x00\x0A'
+    read_resp = b'\x76\x00\x32\x00\x00'
+    read_start = b'\xFF\xD8'
+    read_end = b'\xFF\xD9'
+
+    # done (stop taking pictures?)
+    done_cmd = b'\x56\x00\x36\x01\x03'
+    done_resp = b'\x76\x00\x36\x00\x00'
+
+    # Baud command
+    baud_cmd = b'\x56\x00\x24\x03\x01'
+    baud_resp = b'\x76\x00\x24\x00\x00'
+
+    # Picture Sizes
+    pic_sizes = {'320x240' : b'\x11',
+                 '640x480' : b'\x00',
+                 '160x120' : b'\x22',}
+
+    baud_rates = { 9600 : b'\xAE\xC8',
+                   19200 : b'\x56\xE4',
+                   38400 : b'\x2A\xF2',
+                   57600 : b'\x1C\x4C',
+                   115200 : b'\x0D\xA6',}
 
     def __init__(self, serial):
         self.serial = serial
         self.clearPort()
+        # If miss on lookup for picture size, return default
+        self.pic_sizes.setdefault('320x240')
 
     def waitForInit(self) :
         self.serial.write(self.pic_cmd)
@@ -34,55 +68,109 @@ class LinkSprite :
 
     def reset(self) :
         """
-        Tries to reset the camera
+        Tries to reset the camera. Need to clean this up
         """
         self.serial.write(self.rst_cmd)
         resp_data = self.serial.read(len(self.rst_resp))
 
         if resp_data == self.rst_resp :
             print('Camera Reset')
+        else :
+            counter = 3
+            while not resp_data == self.rst_resp and counter > 0 :
+                print('Trying Reset Again')
+                self.serial.write(self.rst_cmd)
+                resp_data = self.serial.read(len(self.rst_resp))
+                counter -= 1
+        time.sleep(3)
 
-        return (len(resp_data), resp_data)
+        return resp_data == self.rst_resp
+
+    # Reads and returns the processed serial data so its only jpg data
+    def readChunk(self, address) :
+        chunk = None
+
+        cmd = self.read_cont + address + self.read_cont_sep + self.read_size + self.read_cont_delay
+        self.serial.write(cmd)
+
+        resp = self.serial.read(self.read_size_inc + (len(self.read_resp) * 2))
+
+        if resp.endswith(self.read_resp) :
+          chunk = resp[5:-5] # Remove the first and last 7 bytes
+
+        return chunk
+
+    def readPicture(self, file) :
+        address = 0
+        resp = b''
+
+        while not resp.strip().endswith(self.read_end) :
+            print('At address %d' % address)
+            addressBytes = address.to_bytes(2, byteorder='big')
+            chunk = self.readChunk(addressBytes)
+            resp += chunk
+            address += self.read_size_inc
+
+            # Check if we were at the end
+            if self.read_end in chunk :
+              break
+
+        end_index = resp.index(self.read_end)
+
+        with open(file, 'w+b') as f :
+            resp = resp[:end_index + 2]
+            print('Bytes in picture : %d' % len(resp))
+            f.write(resp)
+            f.close()
+
+    def getPicDimBytes(self, size):
+         size_bytes = self.pic_sizes.get(size)
+
+         # Missing Key, we don't have that size
+         if size_bytes is None :
+             print('Warning! Camera does not support %s' % size)
+             print ('Using default 320x240')
+             size_bytes = self.pic_sizes.get('320x240')
+
+         return size_bytes
+
+    def setPicDim(self, size):
+        size_byte = self.getPicDimBytes(size)
+        dim_cmd = self.pic_dim_cmd + size_byte
+
+        self.serial.write(dim_cmd)
+        resp = self.serial.read(len(self.pic_dim_resp))
+        return resp == self.pic_dim_resp
 
     def setCompression(self, compression) :
-        """
-        Sets the compression ration for the jpg image. Expecting values of
+        """ Sets the compression ration for the jpg image. Expecting values of
         0 - 255
         """
         if compression > 255 :
             compression = 255
 
         comp_bytes = compression.to_bytes(1, byteorder='big')
-        print(list(self.comp_cmd + comp_bytes))
+        # print(list(self.comp_cmd + comp_bytes))
         self.serial.write(self.comp_cmd + comp_bytes)
+        resp = self.serial.read(len(self.comp_resp))
 
-        resp = self.serial.read(len(self.comp_resp) + 2)
-        print('Compression resp')
-        print(list(resp))
-        resp_comp = int.from_bytes(resp[-1:], byteorder='big')
-        print(resp_comp)
-
-        return(resp_comp, resp)
-
+        return resp == self.comp_resp
 
     # Try to clear anything thats in the serialPort buffer
     def clearPort(self):
-        """
-        Tries to empty the buffer for the camera. Just in case there's any garbage
+        """Tries to empty the buffer for the camera. Just in case there's any garbage
         """
         while self.serial.in_waiting :
             self.serial.read(self.serial.in_waiting)
         print('Bytes in waiting %d' % self.serial.in_waiting)
 
     def getWaiting(self):
-        """
-        Returns how many bytes are waiting to be read in the serial buffer.
+        """Returns how many bytes are waiting to be read in the serial buffer.
         """
         return self.serial.in_waiting
 
     def getPicSize(self) :
-        """
-        Return the size of the picture that is sitting in the buffer of the
+        """ Return the size of the picture that is sitting in the buffer of the
         camera. Read this many bytes later
         """
         self.serial.write(self.pic_size_cmd)
@@ -96,3 +184,37 @@ class LinkSprite :
         if resp == self.pic_resp :   # Successful
             print('Snap!')
         return(len(resp), resp)
+
+    def getBaudBytes(self, baud):
+        """ Returns the last two bytes for the baud command
+        """
+        baud_bytes = self.baud_rates.get(baud)
+        if baud_bytes is None :
+            baud_bytes = self.baud_rates.get(38400)
+
+        return baud_bytes
+
+    def changeBaud(self, baud) :
+        """ Attempts to change the baud rate of the camera. I don't know when to
+        switch..
+        """
+        success = False
+        baud_bytes = self.getBaudBytes(baud)
+        baud_cmd = self.baud_cmd + baud_bytes
+
+        self.serial.write(baud_cmd)  # Try to change the cameras baud
+        resp = self.serial.read(len(self.baud_resp))
+
+        if resp == self.baud_resp:
+            self.serial.baudrate = baud # update the baudrate
+            success = True
+
+        return success
+
+
+    def done(self) :
+        """ Tells the Camera to stop taking pictures?
+        """
+        self.serial.write(self.done_cmd)
+        resp = self.serial.read(len(self.done_resp))
+        return resp == self.done_resp
--- testLinksprite.py
+++ testLinksprite.py
@@ -2,15 +2,51 @@ import unittest
 import serial
 from LinkSprite import LinkSprite
 import time
+import os.path
 
-serial = serial.Serial('/dev/ttyUSB0', 38400, timeout=1)
+current_baud = 38400
+
+serial = serial.Serial('/dev/ttyUSB1', current_baud, timeout=1)
+# serial = serial.Serial('/dev/tty.usbserial-A5027XNU', 38400, timeout=1)
 
 class TestLinkSprite(unittest.TestCase):
     def setUp(self):
         self.ls = LinkSprite(serial)
         self.ls.clearPort()
         self.ls.reset()
-        time.sleep(1)
+        time.sleep(.1)
+
+    def tearDown(self) :
+        self.ls.reset()
+        self.ls.serial.baudrate = current_baud
+
+    def getImageDimensions(self, f):
+        """ A utility method to make sure the dimensions of the jpg file were
+        set over serial correctly
+        """
+        sof0 = b'\xFF\xC0'  # Marker for meta info about the jpg
+        height_offset = 5 # two byte width
+        width_offset = 7  # two byte width
+
+        pic_height = None
+        pic_width = None
+
+        with open(f, 'r+b') as pic_file :
+            pic_data = pic_file.read()
+            index = pic_data.index(sof0)
+
+            height_index = index + height_offset
+            width_index = index + width_offset
+
+            pic_height = int.from_bytes(pic_data[height_index : height_index + 2], byteorder='big')
+            pic_width = int.from_bytes(pic_data[width_index : width_index + 2], byteorder='big')
+
+            pic_file.close()
+
+        print(str(pic_width) + 'x' + str(pic_height))
+        return (pic_width, pic_height)
+
+
 
     def testClassInitiation(self):
         self.assertFalse(self.ls is None)
@@ -31,15 +67,51 @@ class TestLinkSprite(unittest.TestCase):
         self.assertIsInstance(resp[0], int)
         self.assertTrue(resp[0] > 0 and resp[0] <= 65535)
 
-    def testGetImageSize(self):
-        sizeBase = b'\x56\x00\x31\x05\x04\x01\x00'
-        sz_320x240 = sizeBase + b'\x19\x11'
-        sz_640x480 = sizeBase + b'\x19\x00'
-        sz_160x120 = sizeBase + b'\x19\x22'
-        sz_resp = b'\x76\x00\x31\x00\x00'
 
-        resp = self.ls.setSize('320x240')
-        self.assertEqual(resp[1], sz_resp)
+    def testSetImageSize(self):
+        """ Test setting the image size over serial. Subsequent pictures Should
+        have their dimensions change accordingly
+        """
+        res = self.ls.setPicDim('160x120')
+        self.ls.reset()
+        self.ls.takePic()
+        self.ls.readPicture('160x120.jpg')
+        dim = self.getImageDimensions('160x120.jpg')
+        self.assertEqual(dim[0], 160)
+        self.assertEqual(dim[1], 120)
+        self.assertTrue(res)
+
+        res = self.ls.setPicDim('320x240')
+        self.ls.reset()
+        self.ls.takePic()
+        self.ls.readPicture('320x240.jpg')
+        dim = self.getImageDimensions('320x240.jpg')
+        self.assertEqual(dim[0], 320)
+        self.assertEqual(dim[1], 240)
+        self.assertTrue(res)
+
+        res = self.ls.setPicDim('640x480')
+        self.ls.reset()
+        self.ls.takePic()
+        self.ls.readPicture('640x480.jpg')
+        dim = self.getImageDimensions('640x480.jpg')
+        self.assertEqual(dim[0], 640)
+        self.assertEqual(dim[1], 480)
+        self.assertTrue(res)
+
+        res = self.ls.setPicDim('12x12')
+        self.ls.reset()
+        self.ls.takePic()
+        self.ls.readPicture('12x12.jpg')
+        dim = self.getImageDimensions('12x12.jpg')
+        self.assertEqual(dim[0], 320)
+        self.assertEqual(dim[1], 240)
+        self.assertTrue(res)
+
+    def testReadPic(self):
+        self.ls.takePic()
+        self.ls.readPicture('pic.jpg')
+        self.assertTrue(os.path.isfile('pic.jpg'))
 
     def testTakePicture(self):
         resp = self.ls.takePic()
@@ -48,18 +120,66 @@ class TestLinkSprite(unittest.TestCase):
         self.assertIsInstance(resp[1], bytes)
         self.assertEqual(resp[0], len(resp[1]))
 
+    def testPictureDimBytes(self) :
+        size_byte = self.ls.getPicDimBytes('320x240')
+        self.assertEqual(size_byte, b'\x11')
+
+        size_byte = self.ls.getPicDimBytes('640x480')
+        self.assertEqual(size_byte, b'\x00')
+
+        size_byte = self.ls.getPicDimBytes('160x120')
+        self.assertEqual(size_byte, b'\x22')
+
+        size_byte = self.ls.getPicDimBytes('132x12') # Should miss and get default
+        self.assertEqual(size_byte, b'\x11')
+
+    def testFlow(self):
+        """
+            Tests the work flow that's supposed to happen....
+        """
+        for n in range(0, 10) :
+            comp_res = self.ls.setCompression(50)
+            time.sleep(.1)
+            self.assertTrue(comp_res)
+            self.ls.takePic()
+            self.ls.readPicture('flow.jpg')
+            self.ls.done()
+
     def testCompressionRatio(self):
-        resp = self.ls.setCompression(54)
+         for n in range(0, 260, 15) :
+             # Set the compression and slleeeeepppppp
+             self.ls.setCompression(n)
+             time.sleep(.1)
 
-        print(resp[0])
-        print(resp[1])
+             self.ls.takePic()
+             self.ls.readPicture('./compression_test/comp' + str(n) + '.jpg')
+             self.assertTrue(self.ls.done())
+             time.sleep(.1)
 
-        self.assertTrue(resp[0] > 0)
-        self.assertTrue(resp[0] == 54)
+    def testBaudBytes(self) :
+        baud_bytes = self.ls.getBaudBytes(38400)
+        self.assertEqual(baud_bytes, b'\x2A\xF2')
 
-        resp = self.ls.setCompression(255)
-        self.assertTrue(resp[0] > 0)
-        self.assertTrue(resp[0] == 255)
+        baud_bytes = self.ls.getBaudBytes(115200)
+        self.assertEqual(baud_bytes, b'\x0D\xA6')
+
+    def testChangeBaudRate(self) :
+        next_baud = 115200
+
+        # Make sure current baud is good
+        resp = self.ls.reset()
+        self.assertTrue(resp)
+
+        # Try changing the baud
+        resp = self.ls.changeBaud(next_baud)
+        self.assertTrue(resp)
+
+        # If we get this far try other things
+        self.ls.setCompression(10)
+        time.sleep(.1)
+        self.ls.takePic()
+        self.ls.readPicture('fastBaud.jpg')
+        self.assertTrue(self.ls.done())
 
 if __name__ == '__main__':
   unittest.main()
